8e6c6cac239a249e187b97e1874c3214e29e86c8
Hasnaa-Nageh <hassnaanageh3@gmail.com>
2025-10-02 23:45:42 +0300

    upload on vercel

--- controller/weather.controller.js
+++ controller/weather.controller.js
@@ -1,11 +1,11 @@
 const getWeatherPrediction = require("./../services/mlService");
+// controller
 const getWeather = async (req, res) => {
   try {
-    const { date, region } = req.query;
+    const { date, region } = req.body; // <-- body مش query
     const result = await getWeatherPrediction(date, region);
     res.status(200).json({ status: "success", data: result });
   } catch (err) {
-    console.error(err);
     res.status(500).json({
       success: false,
       message: "Server Error",
