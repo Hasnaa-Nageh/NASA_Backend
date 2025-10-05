8817dae22e617e7b532b80ee68ba19a1b7a662ca
Hasnaa-Nageh <hassnaanageh3@gmail.com>
2025-10-05 16:20:03 +0300

    first commit

--- controller/weather.controller.js
+++ controller/weather.controller.js
@@ -1,9 +1,7 @@
 const getWeatherPrediction = require("./../services/mlService");
 const getWeather = async (req, res) => {
   try {
-    const { region } = req.body;
-    const date = new Date().toISOString().split("T")[0];
-
+    const { date, region } = req.body; 
     const result = await getWeatherPrediction(date, region);
     res.status(200).json({ status: "success", data: result });
   } catch (err) {
