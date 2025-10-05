f47f5ce367c2f441ad7d288b8e6f1f11a7740446
Hasnaa-Nageh <hassnaanageh3@gmail.com>
2025-10-05 15:53:37 +0300

    first commit

--- routes/weather.route.js
+++ routes/weather.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const getWeather = require("./../controller/weather.controller");
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/weather", (req, res) => {
   res.json({ message: "Server Running Successfully" });
 });
 router.post("/weather", getWeather);
